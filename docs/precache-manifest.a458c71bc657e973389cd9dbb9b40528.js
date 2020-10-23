b93de4cfc4aa8bea289c5f5a4361238fb6a554d6
Julian Osorio <julianosorio17@gmail.com>
2020-10-23 18:30:10 -0500

    Docs agregado

---
+++ docs/precache-manifest.a458c71bc657e973389cd9dbb9b40528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe378b333686c8c3937d024bb175475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c2b1b7b991d8b9fc13d",
+    "url": "/static/css/main.897692be.chunk.css"
+  },
+  {
+    "revision": "1b9ea6da0ea3cf667ed7",
+    "url": "/static/js/2.a188440b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.a188440b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2b1b7b991d8b9fc13d",
+    "url": "/static/js/main.240d9bda.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
